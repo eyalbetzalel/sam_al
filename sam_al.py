9833ac7e8375937483b3cc6edbb5a8110c70a3ab
root <eyalbatzul@gmail.com>
2024-05-03 23:41:35 +0000

    add_batch_split

--- sam_al.py
+++ sam_al.py
@@ -1,9 +1,4 @@
 
-# TODO 6 : Evaluate the model's performance unsupervisedly on the data
-# TODO 7 : Implement active learning strategy (e.g., uncertainty sampling, query-by-committee, etc.)
-# TODO 9 : Annotate the selected instances unsupervisedly
-# TODO 10: Retrain the model with the newly annotated instances (fine-tuning SAM)
-# TODO 11: Repeat steps 6-10 until satisfactory performance is achieved
 
 # Fine tune the model with the newly annotated instances : 
 
@@ -99,99 +94,102 @@ def setup_sam_model():
     predictor = SamPredictor(sam)
     return predictor, sam
 
+def get_values_from_data_iter(data, batch_size, predictor, input_image_size=(1024, 2048)):
+
+    # All masks from image to tensor:
+    gt_mask = []
+    for item in data:
+        polygon = item['polygon']
+        mask = polygon_to_mask(polygon)
+        gt_mask.append(mask)
+    gt_mask = torch.tensor(gt_mask).to(predictor.device)
+
+    # Create list of bboxes:
+    bboxes = []
+    for item in data:
+        bbox = item['bbox']
+        bboxes.append(bbox)
+    bboxes = torch.tensor(bboxes, device=predictor.device)
+    bboxes = predictor.transform.apply_boxes_torch(bboxes, input_image_size)
+
+    # Create a list of labels:
+    labels = []
+    for item in data:
+        label = item['label']
+        labels.append(label)
+    
+    gt_mask_split = torch.split(gt_mask, batch_size, dim=0)
+    bboxes_split = torch.split(bboxes, batch_size, dim=0)
+    labels_split = [labels[i:i+batch_size] for i in range(0, len(labels), batch_size)]
+    
+    return gt_mask_split, bboxes_split, labels_split
+
 def finetune_sam_model(dataset, batch_size=4, epoches=10):
 
     # Create a DataLoader instance for the training dataset
     from torch.utils.data import DataLoader
     from torch.nn.functional import threshold, normalize
 
-
     optimizer = torch.optim.Adam(sam_model.mask_decoder.parameters())
     loss_fn = torch.nn.MSELoss()
 
-    for input_image, data in dataset:
-        # data is a list of dictionaries with keys 'label', 'polygon', and 'bbox'. this code do the following:
-        # 1. Convert the polygon to a mask and put it in a tensors (gt_mask)
-        # 2. Create list of bboxes
-        # 3. Create a list of labels
+    for epoch in range(epoches):
+        for index, (input_image, data) in enumerate(dataset):
 
-        # Convert the polygon to a mask and put it in a tensors (gt_mask):
-        input_image = input_image.to(predictor.device)
-        
-        original_image_size = (1024, 2048)
-        input_size = (1024, 2048)
-        gt_mask = []
-        for item in data:
-            polygon = item['polygon']
-            mask = polygon_to_mask(polygon)
-            gt_mask.append(mask)
-        gt_mask = torch.tensor(gt_mask).to(predictor.device)
+            input_image = input_image.to(predictor.device)
+            original_image_size = (1024, 2048)
+            input_size = (1024, 2048)            
+            with torch.no_grad():
 
-        # Create list of bboxes:
-        bboxes = []
-        for item in data:
-            bbox = item['bbox']
-            bboxes.append(bbox)
-        bboxes = torch.tensor(bboxes, device=predictor.device)
-
-        # Create a list of labels:
-        labels = []
-        for item in data:
-            label = item['label']
-            labels.append(label)
-        
-        # Set the image to the predictor
-
-        transformed_boxes = predictor.transform.apply_boxes_torch(bboxes, input_image.shape[:2])
-        
-        with torch.no_grad():
-
-            """ We want to embed images by wrapping the encoder in the torch.no_grad() 
-            context manager, since otherwise we will have memory issues, along with 
-            the fact that we are not looking to fine-tune the image encoder. """
-            # add another false dimension to input_image
-            input_image = input_image.unsqueeze(0)
-            input_image_postprocess = sam_model.preprocess(input_image)
-            image_embedding = sam_model.image_encoder(input_image_postprocess)
-        
-        with torch.no_grad():
+                """ We want to embed images by wrapping the encoder in the torch.no_grad() 
+                context manager, since otherwise we will have memory issues, along with 
+                the fact that we are not looking to fine-tune the image encoder. """
+                # add another false dimension to input_image
+                input_image = input_image.unsqueeze(0)
+                input_image_postprocess = sam_model.preprocess(input_image)
+                image_embedding = sam_model.image_encoder(input_image_postprocess)
             
-            """ We can also generate the prompt embeddings within the no_grad context manager. 
-            We use our bounding box coordinates, converted to pytorch tensors.
-            """
+            gt_mask, bboxes, labels = get_values_from_data_iter(data, batch_size, predictor)
+                        
+            for i, (curr_gt_mask, curr_bbox, curr_label) in enumerate(zip(gt_mask, bboxes, labels)):
+
+                with torch.no_grad():
+                    
+                    """ We can also generate the prompt embeddings within the no_grad context manager. 
+                    We use our bounding box coordinates, converted to pytorch tensors.
+                    """
+                    
+                    sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
+                        points=None,
+                        boxes=curr_bbox,
+                        masks=None,
+                    )
+                """ Finally, we can generate the masks. Note that here we are in single mask generation
+                    mode (in contrast to the 3 masks that are normally output).""" 
+                
+                low_res_masks, iou_predictions, upscaled_embedding = sam_model.mask_decoder(
+                image_embeddings=image_embedding,
+                image_pe=sam_model.prompt_encoder.get_dense_pe(),
+                sparse_prompt_embeddings=sparse_embeddings,
+                dense_prompt_embeddings=dense_embeddings,
+                multimask_output=False,
+                )
+
+                """ The final step here is to upscale the masks back to the original image size since they are low resolution.
+                We can use Sam.postprocess_masks to achieve this. We will also want to generate binary masks from the 
+                predicted masks so that we can compare these to our ground truths. It is important to use torch functionals 
+                in order to not break backpropagation."""
             
-            sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
-                points=None,
-                boxes=transformed_boxes[:4,:],
-                masks=None,
-            )
-        """ Finally, we can generate the masks. Note that here we are in single mask generation
-            mode (in contrast to the 3 masks that are normally output).""" 
-        
-        low_res_masks, iou_predictions, upscaled_embedding = sam_model.mask_decoder(
-        image_embeddings=image_embedding,
-        image_pe=sam_model.prompt_encoder.get_dense_pe(),
-        sparse_prompt_embeddings=sparse_embeddings,
-        dense_prompt_embeddings=dense_embeddings,
-        multimask_output=False,
-        )
+                upscaled_masks = sam_model.postprocess_masks(low_res_masks, input_size, original_image_size).to(predictor.device)
 
-        """ The final step here is to upscale the masks back to the original image size since they are low resolution.
-         We can use Sam.postprocess_masks to achieve this. We will also want to generate binary masks from the 
-         predicted masks so that we can compare these to our ground truths. It is important to use torch functionals 
-         in order to not break backpropagation."""
-    
-        upscaled_masks = sam_model.postprocess_masks(low_res_masks, input_size, original_image_size).to(predictor.device)
+                binary_mask = normalize(threshold(upscaled_masks, 0.0, 0)).to(predictor.device)
+                binary_mask = binary_mask.squeeze()
 
-        binary_mask = normalize(threshold(upscaled_masks, 0.0, 0)).to(predictor.device)
-        binary_mask = binary_mask.squeeze()
-        gt_mask = gt_mask[:4,:]
-
-        loss = loss_fn(binary_mask, gt_mask.float())
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        print(f"Loss: {loss.item()}")
+                loss = loss_fn(binary_mask, curr_gt_mask.float())
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                print(f"Loss: {loss.item()} | Epoch: {epoch} | Index: {index} from {len(dataset)} | Batch: {i} from {len(gt_mask)}")
 
 predictor, sam_model = setup_sam_model()
 iou_dict = {}
@@ -228,5 +226,13 @@ high_flag = True
         
 active_learning_dataset = ActiveLearningDataset(train_dataset, 0.1)
 training_subset = active_learning_dataset.get_training_subset()
-finetune_sam_model(dataset=training_subset, batch_size=4, epoches=10)
+finetune_sam_model(dataset=training_subset, batch_size=16, epoches=10)
+
+# TODO 1 : Save model after iteration of traning on random subset of data
+# TODO 2 : Evaluate the model's performance on the validation data
+# TODO 3 : Implement active learning strategy 
+# TODO 4 : Evaluate the model's performance unsupervisedly on the data
+# TODO 5 : Annotate the selected instances unsupervisedly (????????)
+# TODO 6: Retrain the model with the newly annotated instances (fine-tuning SAM)
+# TODO 7: Repeat steps 6-10 until satisfactory performance is achieved
 v=0
